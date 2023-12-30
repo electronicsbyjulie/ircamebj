174ae0347229078d4a85cdbc5262e1c17864b6ea
Electronics by Julie <66374250+electronicsbyjulie@users.noreply.github.com>
2023-12-30 09:14:20 -0700

    Application ID.

--- ctrlr2.c
+++ ctrlr2.c
@@ -4191,7 +4191,7 @@ int main (int argc, char **argv)
     
     tmdat = thermdat;
 	
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("exj.ircamebj.ctrlr", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
